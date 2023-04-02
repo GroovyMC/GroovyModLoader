1ed9adf083b5cd3b24289e6ebaf6ae0004f4232f
Matyrobbrt <matyrobbrt@gmail.com>
2023-04-02 17:57:26 +0300

    Document `GModEventBus`

--- src/main/groovy/com/matyrobbrt/gml/bus/GModEventBus.groovy
+++ src/main/groovy/com/matyrobbrt/gml/bus/GModEventBus.groovy
@@ -9,6 +9,9 @@ import groovy.transform.Canonical
 import groovy.transform.CompileStatic
 import net.minecraftforge.eventbus.api.IEventBus
 
+/**
+ * An {@link IEventBus} that may provide some groovy-specific methods in the future.
+ */
 @Canonical
 @CompileStatic
 final class GModEventBus implements IEventBus {
